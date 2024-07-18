7bed5b3152eaa02f0711380ca008a58cc6547b9d
Advik <advik.b@gmail.com>
2024-07-18 22:24:40 +0530

    Update main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <ModrinthAPI.hpp>
+#include <ModrinthProject.hpp>
+#include <nlohmann/json.hpp>
+
+using nlohmann::json;
 
 int main() {
     std::cout << modrinth::version << std::endl;
-    
+    json j;
+    j["id"] = "test";
+    auto project = modrinth::Project::from_json(j);
     return 0;
 }
